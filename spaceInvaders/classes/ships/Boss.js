917a376dcceec8aaab1e09bdf4159399ae8aaf8c
Konstan <55129891+1800Kon@users.noreply.github.com>
2022-03-20 15:05:31 +0100

    remove

--- spaceInvaders/classes/ships/Boss.js
+++ spaceInvaders/classes/ships/Boss.js
@@ -1,5 +1,20 @@
-class Boss extends Ship {
+class Boss extends ShipDecorator {
+
     constructor(position, velocity, hitboxSize, sprite) {
         super(position, velocity, hitboxSize, sprite);
+    }
+
+    saveState() {
+        if (condition) {
+
+        }
+        return new Memento(this.state)
+    }
+
+    restoreRandomState() {
+        if (condition) {
+
+        }
+        this.state = Memento.getSavedState();
     }
 }
