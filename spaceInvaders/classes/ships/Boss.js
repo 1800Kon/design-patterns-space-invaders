3db07e9c44f1336586759fb76fe7d714118da5fd
Konstan <55129891+1800Kon@users.noreply.github.com>
2022-03-20 15:07:09 +0100

    Update Boss.js

--- spaceInvaders/classes/ships/Boss.js
+++ spaceInvaders/classes/ships/Boss.js
@@ -1,20 +1,5 @@
 class Boss extends ShipDecorator {
-
     constructor(position, velocity, hitboxSize, sprite) {
         super(position, velocity, hitboxSize, sprite);
-    }
-
-    saveState() {
-        if (condition) {
-
-        }
-        return new Memento(this.state)
-    }
-
-    restoreRandomState() {
-        if (condition) {
-
-        }
-        this.state = Memento.getSavedState();
     }
 }
